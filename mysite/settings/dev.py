bd8e1a2bee41e789ee5c5b66baa51ef408bff612
pavanaipl <pavan@appinessworld.com>
2020-05-15 15:43:55 +0530

    added gitignore file

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '41&lu+_^m4c_=t^w^r9(8+&ptj!dzfw568p&ic7awp64a(5uw^'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ testapp/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel, RichTextFieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+from wagtail.search import index
+
+# Create your models here.
+
+class UsersDetails(Page):
+
+    name = models.CharField(max_length=250, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    mobile_number = models.CharField(max_length=11, null=True, blank=True)
+    age = models.IntegerField(default=0)
+    dob = models.DateField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full"),
+        FieldPanel('name'),
+        FieldPanel('email'),
+        FieldPanel('mobile_number'),
+        FieldPanel('age'),
+
+    ]
+    def __str__(self):
+        return self.name
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
